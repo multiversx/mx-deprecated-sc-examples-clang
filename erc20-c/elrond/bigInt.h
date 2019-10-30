a55e0a29da6ec123581d1c6a3d8a95f5fcdb81fd
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-10-30 22:01:03 +0200

    Fix formatting

--- erc20-c/elrond/bigInt.h
+++ erc20-c/elrond/bigInt.h
@@ -2,26 +2,26 @@
 
 typedef unsigned int bigInt;
 
-bigInt		bigIntNew(int value);
+bigInt    bigIntNew(int value);
 
-void			bigIntgetArgument(int argumentIndex, bigInt argument);
+void      bigIntgetArgument(int argumentIndex, bigInt argument);
 
-int				bigIntstorageLoad(byte *key, bigInt value);
-int				bigIntstorageStore(byte *key, bigInt value);
+int       bigIntstorageLoad(byte *key, bigInt value);
+int       bigIntstorageStore(byte *key, bigInt value);
 
-void			bigIntAdd(bigInt destination, bigInt op1, bigInt op2);
-void			bigIntSub(bigInt destination, bigInt op1, bigInt op2);
-void			bigIntMul(bigInt destination, bigInt op1, bigInt op2);
-int				bigIntCmp(bigInt op1, bigInt op2);
+void      bigIntAdd(bigInt destination, bigInt op1, bigInt op2);
+void      bigIntSub(bigInt destination, bigInt op1, bigInt op2);
+void      bigIntMul(bigInt destination, bigInt op1, bigInt op2);
+int       bigIntCmp(bigInt op1, bigInt op2);
 
-int				bigIntIsInt64(bigInt reference);
+int       bigIntIsInt64(bigInt reference);
 long long bigIntGetInt64(bigInt reference);
-void			bigIntSetInt64(bigInt destination, long long value);
+void      bigIntSetInt64(bigInt destination, long long value);
 
-void			bigIntFinish(bigInt reference);
-void			bigIntgetCallValue(bigInt destination);
-void			bigIntgetExternalBalance(byte *address, bigInt result);
+void      bigIntFinish(bigInt reference);
+void      bigIntgetCallValue(bigInt destination);
+void      bigIntgetExternalBalance(byte *address, bigInt result);
 
-int				bigIntByteLength(bigInt reference);
-int				bigIntGetBytes(bigInt reference, byte *byte);
-void			bigIntSetBytes(bigInt destination, byte *byte, int byteLength);
+int       bigIntByteLength(bigInt reference);
+int       bigIntGetBytes(bigInt reference, byte *byte);
+void      bigIntSetBytes(bigInt destination, byte *byte, int byteLength);
