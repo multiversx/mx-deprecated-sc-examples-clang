45f97a106e7606703502dd1502a8fdaf4ce3f0de
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-07-09 21:26:54 +0300

    Add snippet for simple counter.

---
+++ simple-counter/snippets.sh
@@ -0,0 +1,21 @@
+DENOMINATION="000000000000000000"
+PROXY="https://api.elrond.com"
+CHAIN="BoN"
+ALICE_PEM="~/wallet/alice.pem"
+CONTRACT_ADDRESS="erd1YourContractHere"
+
+deploy() {
+    erdpy --verbose contract deploy ${CONTRACT_FOLDER} --recall-nonce --pem=${ALICE_PEM} --gas-limit=5000000 --proxy=${PROXY} --chain=${CHAIN}
+}
+
+increment() {
+    erdpymy --verbose contract call ${CONTRACT_ADDRESS} --recall-nonce --pem=${ALICE_PEM} --gas-limit=5000000 --function="increment" --proxy=${PROXY} --chain=${CHAIN}
+}
+
+decrement() {
+    erdpy --verbose contract call ${CONTRACT_ADDRESS} --recall-nonce --pem=${ALICE_PEM} --gas-limit=5000000 --function="decrement" --proxy=${PROXY} --chain=${CHAIN}
+}
+
+get() {
+    erdpymy --verbose contract query ${CONTRACT_ADDRESS} --function="get" --proxy=${PROXY}
+}
