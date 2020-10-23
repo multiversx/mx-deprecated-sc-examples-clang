1d41fd694287b28959ba06ac581662f5662e9b69
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-10-23 21:54:06 +0300

    Improve snippets.

--- simple-counter/snippets.sh
+++ simple-counter/snippets.sh
@@ -1,17 +1,29 @@
 ALICE="${USERS}/alice.pem"
-CONTRACT_ADDRESS="erd1YourContractHere"
+ADDRESS=$(erdpy data load --key=address)
+TRANSACTION=$(erdpy data load --key=deployTransaction)
 
 deploy() {
-    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=5000000
+    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --send --outfile="deploy.json"
+
+    TRANSACTION=$(erdpy data parse --file="deploy.json" --expression="data['hash']")
+    ADDRESS=$(erdpy data parse --file="deploy.json" --expression="data['address']")
+    erdpy data store --key=address --value=${ADDRESS}
+    erdpy data store --key=deployTransaction --value=${TRANSACTION}
+}
+
+checkDeployment() {
+    erdpy tx get --hash=$TRANSACTION --omit-fields="['data', 'signature']"
+    erdpy account get --address=$ADDRESS --omit-fields="['code']"
+}
 
 increment() {
-    erdpy --verbose contract call ${CONTRACT_ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --function="increment"
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --function="increment" --send
 }
 
 decrement() {
-    erdpy --verbose contract call ${CONTRACT_ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --function="decrement"
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --function="decrement" --send
 }
 
 get() {
-    erdpy --verbose contract query ${CONTRACT_ADDRESS} --function="get"
+    erdpy --verbose contract query ${ADDRESS} --function="get"
 }
